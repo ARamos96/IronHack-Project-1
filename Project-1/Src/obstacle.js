6bd2ae6ba096310669e4e92eecde8ec4659e221b
Arnau Ramos <arnauramos21@gmail.com>
2024-02-15 21:30:29 +0100

    started 1 obstacle class

---
+++ Project-1/Src/obstacle.js
@@ -0,0 +1,8 @@
+class downwardsObstacle {
+    constructor(gameScreen) {
+        this.gameScreen = gameScreen
+        this.left = this.gameScreen.offsetWidth - 80
+        this.top = 0
+        this.width
+    }
+}

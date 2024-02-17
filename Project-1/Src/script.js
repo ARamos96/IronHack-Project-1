f387531e628a5768512e93e23c5a9da53b25527a
Arnau Ramos <arnauramos21@gmail.com>
2024-02-17 15:36:34 +0100

    created window.onload function

--- Project-1/Src/script.js
+++ Project-1/Src/script.js
@@ -1,1 +1,27 @@
-window.onload()
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game;
+
+  function startGame() {
+    game = new Game();
+
+    game.start();
+  }
+
+  startButton.onclick = function () {
+    startGame();
+  };
+
+  function restartGame() {
+    location.reload();
+  }
+
+  restartButton.onclick = function () {
+    restartGame();
+  };
+
+  startButton.onclick = function () {
+    startGame();
+  };
+};

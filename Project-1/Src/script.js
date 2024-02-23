761ea5b8b2e22c0bee115385e63a1982893f609d
Arnau Ramos <arnauramos21@gmail.com>
2024-02-23 17:45:46 +0100

    added jump sound effect

--- Project-1/Src/script.js
+++ Project-1/Src/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  const jumpSound = document.getElementById('jump-sound')
   let game;
 
   function startGame() {
@@ -24,6 +25,7 @@ window.onload = function () {
   window.addEventListener("keydown", function (event) {
     if (event.code == "Space") {
       game.player.directionY = -12;
+      jumpSound.play()
     }
   });
 
@@ -31,6 +33,7 @@ window.onload = function () {
   window.addEventListener("keyup", function (event) {
     if (event.code == "Space") {
       game.player.directionY = 0;
+      jumpSound.load()
     }
   });
 };
